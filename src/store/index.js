6f41b17dc5d75234f8aad4e6f41517c285611447
Jamal Hasuev <metafra@mail.ru>
2021-07-16 15:29:52 +0300

    add post for test

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,59 @@ import { createStore } from 'vuex'
 
 const store = createStore({
   state: {
+    posts: [
+      {
+        id: 1,
+        title: "заголовок 1",
+        excerpt: "краткое описание 1",
+        text: "полное описание 1",
+      },
+      {
+        id: 2,
+        title: "заголовок 2",
+        excerpt: "краткое описание 2",
+        text: "полное описание 2",
+      },
+      {
+        id: 3,
+        title: "заголовок 3",
+        excerpt: "краткое описание 3",
+        text: "полное описание 3",
+      },
+    ],
+    comments: [
+      {
+        id: 1,
+        postID: 1,
+        username: "Jamal",
+        text: "This is a comment...",
+      },
+      {
+        id: 2,
+        postID: 2,
+        username: "Jamal 2",
+        text: "This is a comment... 2",
+      },
+      {
+        id: 3,
+        postID: 1,
+        username: "Jamal 3",
+        text: "This is a comment... 3",
+      },
+    ],
   },
   
   getters: {
+    getPosts(state) {
+      return state.posts.filter(post => {
+        // количество комментов у поста
+        post.commentsCount = state.comments.reduce((acc, comment) => {
+          return post.id === comment.postID ? acc + 1 : acc
+        }, 0)
+
+        return post
+      })
+    },
   },
 
   mutations: {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,31 @@
 <template>
-  Home.vue
+  <h1 class="text-center mb-4"> Посты </h1>
+
+  <div
+    v-for="post in getPosts"
+    :key="post.id"
+    class="card  mb-3"
+  >
+    <div class="card-body">
+      <h5 class="card-title">{{ post.title }}</h5>
+      <p class="card-text">{{ post.excerpt }}</p>
+      <div class="d-flex justify-content-between align-items-center">
+        <span> Комментариев: {{ post.commentsCount }} </span>
+        <button class="btn btn-primary">Читать</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex"
 export default ({
   name: "Home",
+  computed: {
+    ...mapGetters([
+      'getPosts',
+      'getComments',
+    ]),
+  },
 })
 </script>
