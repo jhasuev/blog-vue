7cef256e15df2dbb6f41d983e19332eb4aa70f47
Jamal Hasuev <metafra@mail.ru>
2021-07-16 14:51:50 +0300

    add bootstrap styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  app...
-  <router-view />
+  <div class="container  py-3">
+    <div class="row">
+      <div class="col">
+        <router-view />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
